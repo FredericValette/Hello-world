7c2d7cad0e8a48cec0c3916908ab94eb951d4fff
Frederic Valette <FredericValette@users.noreply.github.com>
2017-05-12 09:14:47 +0200

    Version for Step 1 - interacting with elements

--- spec.js
+++ spec.js
@@ -1,8 +1,13 @@
 // spec.js
 describe('Protractor Demo App', function() {
-  it('should have a title', function() {
+  it('should add one and two', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
+    element(by.model('first')).sendKeys(1);
+    element(by.model('second')).sendKeys(2);
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+    element(by.id('gobutton')).click();
+
+    expect(element(by.binding('latest')).getText()).
+        toEqual('3'); // This is wrong!
   });
 });
